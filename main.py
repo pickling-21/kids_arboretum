081fc66d6b03be7a0949ef8377751016ea49985a
Pickling <marchelimka@gmail.com>
2023-07-28 16:45:41 +0300

    a

--- main.py
+++ main.py
@@ -2,34 +2,39 @@
 from telebot import TeleBot, types
 from random import randint
 # токен для бота
-token = "6365845419:AAGzeRqMSyEFhoGQwJ4pdFd5f-R0kvGjtSI"
-
+from secret import token
+privet_word = "Добро пожаловать в квест"
 # словарь картинок для квеста
+
 pictures = {
-    0: "https://storage.geekclass.ru/images/760e484b-a099-4a7a-a722-5aec9a933614.jpg",
-    1: "https://storage.geekclass.ru/images/4637fc41-08df-466a-b112-aa577dba6c1d.jpg",
-    2: "https://storage.geekclass.ru/images/c2a2a60c-9c7b-4c3a-b663-42d2559bf869.jpg"
-}
+    0: "https://kmv-tur.org/sites/default/files/node/camp/field_images3/2021-05/be6cf54956661e553242225d536ada4a.jpg",
+    1: "https://img.tourister.ru/files/2/1/0/7/2/7/6/1/original.jpg",
+    2: "https://img.ixbt.site/live/topics/preview/00/03/75/54/74b9058c4c.jpg",
+    3: "https://mylnaya-opera.ru/481-large_default/zagotovka-dlya-dekupazha-nadpis-informaciya.jpg",
+    4: "https://mylnaya-opera.ru/481-large_default/zagotovka-dlya-dekupazha-nadpis-informaciya.jpg"}
 
 states = {}
 inventories = {}
 bot = TeleBot(token)
 
+
 @bot.message_handler(commands=["start"])
 def start_game(message):
     user = message.chat.id
-
+    # получаем уникальный id юзера
     states[user] = 0
     inventories[user] = []
 
-    bot.send_message(user, "Добро пожаловать в игру!")
+    bot.send_message(user, privet_word)
 
     process_state(user, states[user], inventories[user])
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def user_answer(call):
     user = call.message.chat.id
     process_answer(user, call.data)
+
 
 def process_state(user, state, inventory):
     kb = types.InlineKeyboardMarkup()
@@ -37,53 +42,77 @@ def process_state(user, state, inventory):
     bot.send_photo(user, pictures[state])
 
     if state == 0:
-        kb.add(types.InlineKeyboardButton(text="пойти направо", callback_data="1"))
-        kb.add(types.InlineKeyboardButton(text="пойти налево", callback_data="2"))
+        # вход
+        kb.add(types.InlineKeyboardButton(
+            text="пойти в дендропарк", callback_data="1"))
+        kb.add(types.InlineKeyboardButton(
+            text="пойти к 1 отряду", callback_data="2"))
 
-        bot.send_message(user, "Вы в оказались в темном подземелье, перед вами два прохода.", reply_markup=kb)
+        bot.send_message(user, "Вы оказались в лагере чайка", reply_markup=kb)
 
     if state == 1:
-        kb.add(types.InlineKeyboardButton(text="переплыть", callback_data="1"))
-        kb.add(types.InlineKeyboardButton(text="вернуться", callback_data="2"))
 
-        bot.send_message(user, "Перед вами большое подземное озеро, а вдали виднеется маленький остров.", reply_markup=kb)
+        kb.add(types.InlineKeyboardButton(
+            text="подойти к фонтану", callback_data="1"))
+        kb.add(types.InlineKeyboardButton(
+            text="пройти к диджей зоне", callback_data="2"))
+
+        bot.send_message(
+            user, "вы в дендропарке", reply_markup=kb)
 
     if state == 2:
-        bot.send_message(user, "Вы выиграли.")
+        kb.add(types.InlineKeyboardButton(
+            text="ответ 1 диджейr", callback_data="1"))
+        kb.add(types.InlineKeyboardButton(
+            text="ответ 2 диджейr", callback_data="2"))
+        bot.send_message(user, "загадка диджей зона")
+
+    if state == 3:
+        kb.add(types.InlineKeyboardButton(
+            text="ответ 1 фонтан ff", callback_data="1"))
+        kb.add(types.InlineKeyboardButton(
+            text="ответ 2 фонтанaa", callback_data="2"))
+        bot.send_message(user, "загадка фонтан")
+
+    if state == 4:
+        bot.send_message(user, "вы у первого отряда")
+        kb.add(types.InlineKeyboardButton(
+            text="вернуться", callback_data="2"))
+
 
 def process_answer(user, answer):
     if states[user] == 0:
         if answer == "1":
             states[user] = 1
-        else:
-            if "key" in inventories[user]:
-                bot.send_message(user,
-                                 "Перед вами закрытая дверь. Вы пробуете открыть ее ключем, и дверь поддается. Кажется, это выход.")
-                states[user] = 2
-            else:
-                bot.send_message(user, "Перед вами закрытая дверь, и, кажется, без ключа ее не открыть. Придется вернуться обратно.")
-                states[user] = 0
+            bot.send_message(user, "!!!!!!!!!!!!!!!!1")
+        elif answer == "2":
+            states[user] = 4
 
     elif states[user] == 1:
-        if answer == "2":
-            bot.send_message(user,
-                             "И правда, не стоит штурмовать неизвестные воды. Возвращаемся назад...")
-            states[user] = 0
-        else:
-            bot.send_message(user,
-                             "Вы пробуете переплыть озеро...")
+        # если фонтан
+        if answer == "1":
+            states[user] = 3
+        # если диджей зона
+        elif answer == "2":
+            states[user] = 2
 
-            chance = randint(0,10)
-            if chance == 4:
-                bot.send_message(user,
-                                 "Вода оказалось теплой, а в сундуке на острове вы нашли старый ключ. Стоит вернутся обратно.")
-                inventories[user].append("key")
-                states[user] = 0
-            else:
-                bot.send_message(user,
-                                 "На середине озера вас подхватывают волны и возвращают обратно.")
-                states[user] = 1
+    # elif states[user] == 2:
+    #     if answer == "1":
+    #         bot.send_message(user,
+    #                          "Правильный ответ")
+    #     elif answer == "2":
+    #         bot.send_message(user,
+    #                          "Неправильный ответ")
+
+    # elif states[user] == 3:
+    #     if answer == "1":
+    #         bot.send_message(user,
+    #                          "Правильный ответ")
+    #     elif answer == "2":
+    #         bot.send_message(user,
+    #                          "Неправильный ответ")
 
     process_state(user, states[user], inventories[user])
+
 
 bot.polling(none_stop=True)
